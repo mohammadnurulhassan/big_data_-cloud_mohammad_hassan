063c393a5bd415d93239009dd62382bbd9886bbe
Mohammad Nurul Hassan <mdnrlhsn@gmail.com>
2025-10-22 22:54:51 +0200

    do update

--- dlt_code/load_job_ads.py
+++ dlt_code/load_job_ads.py
@@ -1,8 +1,23 @@
 import dlt
 import requests
 import json
-from pathlib import Path
+#from pathlib import Path
 import os
+
+
+dlt.config["load.truncate_staging_dataset"] = True
+
+
+
+ params = {"q": query, "limit": 100, "occupation-field": occupation_field}
+query = ""
+    table_name = "job_ads"
+
+
+    occupation_fields = ("yhCP_AqT_tns", "Uuf1_GMh_Uvw", "9puE_nYg_crq")
+
+
+
 
 def _get_ads(url_for_search, params):
     headers = {"accept": "application/json"}
@@ -11,7 +26,7 @@ def _get_ads(url_for_search, params):
     return json.loads(response.content.decode("utf8"))
 
 
-@dlt.resource(write_disposition="merge", primary_key="ID")
+@dlt.resource(table_name="job_ads",write_disposition="merge", primary_key="ID")
 def jobsearch_resource(params):
     """
     params should include at least:
@@ -44,30 +59,84 @@ def jobsearch_resource(params):
         offset += limit
 
 
-def run_pipeline(query, table_name, occupation_fields):
-    pipeline = dlt.pipeline(
-        pipeline_name="jobads_demo",
-        destination="snowflake",
-        dataset_name="staging",
-    )
 
-    for occupation_field in occupation_fields:
-        params = {"q": query, "limit": 100, "occupation-field": occupation_field}
-        load_info = pipeline.run(
-            jobsearch_resource(params=params), table_name=table_name
-        )
-        print(f"Occupation field: {occupation_field}")
-        print(load_info)
+#dagster works with dlt source not dlt resource
+
+@dlt.source
+def jobads_source(q):
+    return jobads_resource(params={"q": q, "limit": 100})
 
 
-if __name__ == "__main__":
-    working_directory = Path(__file__).parent
-    os.chdir(working_directory)
-
-    query = ""
-    table_name = "job_ads"
 
 
-    occupation_fields = ("yhCP_AqT_tns", "Uuf1_GMh_Uvw", "9puE_nYg_crq")
 
-    run_pipeline(query, table_name, occupation_fields)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#def run_pipeline(query, table_name, occupation_fields):
+   # pipeline = dlt.pipeline(
+   #     pipeline_name="jobads_demo",
+   #     destination="snowflake",
+   #     dataset_name="staging",
+   # )
+   #
+   # for occupation_field in occupation_fields:
+   #     params = {"q": query, "limit": 100, "occupation-field": occupation_field}
+   #     load_info = pipeline.run(
+   #         jobsearch_resource(params=params), table_name=table_name
+   #     )
+   #     print(f"Occupation field: {occupation_field}")
+   #     print(load_info)
+
+   # for occupation_field in occupation_fields:
+       
+       # load_info = pipeline.run(
+       #     jobsearch_resource(params=params), table_name=table_name
+       # )
+       # print(f"Occupation field: {occupation_field}")
+       # print(load_info)
+
+
+#if __name__ == "__main__":
+   # working_directory = Path(__file__).parent
+   # os.chdir(working_directory)
+
+    
+   # run_pipeline(query, table_name, occupation_fields)
